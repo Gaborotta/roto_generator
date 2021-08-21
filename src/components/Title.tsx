f3befa8750e06a7896d33f5591b246859e7aeb9a
gaborotta <10.daaaan.24@gmail.com>
2021-08-21 23:45:57 +0900

    サブタイトル修正

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -4,7 +4,7 @@ const Title = () => {
   return (
     <div>
       <h1 className="title">ロト ジェネレータ</h1>
-      <h2 className="description">ロトの数値をランダム生成</h2>
+      <h2 className="description">ロトの数字をランダム生成</h2>
     </div>
   );
 };
